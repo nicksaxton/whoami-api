eb39d175f069a13aa3af236ec0f816b83e4b33ac
Nick <nick.saxton@gmail.com>
2017-01-04 15:57:18 +0000

    Working header info parser

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ app.set('port', (process.env.PORT || 8080))
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    var headerInfo = {
+        'ipaddress': req.headers['x-forwarded-for'],
+        'language': req.headers['accept-language'].split(',')[0],
+        'software': req.headers['user-agent'].split('(')[1].split(')')[0]
+    }
+    
+    res.send(JSON.stringify(headerInfo))
 })
 
 app.listen(app.get('port'), function () {
